57c2f0cd961fc94146c1dd904bfdebaf97a60307
OtienohOwinoh <coowinoh@gmail.com>
2022-10-21 00:54:23 +0300

    commit tree 6ee3b571efcbba8420fa6d6077e098ae3512acf5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,46 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+
+end
+
+greet
+
+greet"Sunny"
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+ add(2, 5)
+
+ def halve(number)
+    return nil unless number.class == Integer
+  
+  number / 2
+end
+
+
+
+
+
+
+
+
+
